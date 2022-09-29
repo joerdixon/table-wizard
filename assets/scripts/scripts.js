39a17906e1e93bae08147dfdc53c5056f4b29626
joerdixon <joseph35dixon@gmail.com>
2022-09-28 18:07:57 -0700

    started pseudocode

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,20 @@
+// PSEUDOCODE
+
+// When the cooper hits the button.
+
+// var d6 = Pick a random number from 1 to 6
+
+// var dice = [];
+
+// for (i = 0; i <= d6; i++) {
+    
+//   var percentile = random number 1-100
+//             if (percentile < 10) {
+//                  add to dice
+//                  BREAK THE FUNCTION
+//                  }
+//      add the value to dice    
+//}
+
+
+function tableGen();
